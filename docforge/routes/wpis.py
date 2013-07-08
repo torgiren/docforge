7e72ee808550feb1dd7d194555043940728381c3
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2013-07-08 21:17:16 +0200

    Możliwość wyświetlania wniosków

--- docforge/routes/wpis.py
+++ docforge/routes/wpis.py
@@ -1,2 +1,4 @@
 def wpis(config):
     config.add_route('wpis_add', '/add', request_method="POST")
+    config.add_route('wpis_list', '/list')
+    config.add_route('wpis_show', '/list/{id}')
--- docforge/views/wpis.py
+++ docforge/views/wpis.py
@@ -1,6 +1,8 @@
 #*-* coding: utf8 *-*
 from pyramid.view import view_config
 from docforge.views.common import startwith
+from docforge.views.formatki import FormatkiView
+from bson import objectid
 
 
 class WpisView(object):
@@ -23,8 +25,34 @@ class WpisView(object):
         self.db.insert(items)
         return {}
 
+    def _list(self, id=None):
+        if id:
+            warunek = {'_formatka': id}
+        else:
+            warunek = None
+        print "Warunek: ", warunek
+        objs = tuple(self.db.find(warunek, {'_formatka': 0, '_id': 0}))
+        print objs
+#        del(objs['_formatka'])
+#        del(objs['_id'])
+        return objs
 
     @view_config(route_name='wpis_add', renderer='string')
     def add(self):
         self._add(self.request.POST)
         return {}
+
+    @view_config(route_name='wpis_list', renderer='docforge:templates/list.jinja2')
+    def list(self):
+        tmp = FormatkiView(self.request)
+        items = [{'verbose': unicode(i['_nazwa']), 'href': self.request.route_path('wpis_show', id=i['_id'])} for i in tmp._list()]
+        return {'items': items, 'title': u'Dostępne formularze'}
+
+#    @view_config(route_name='wpis_show', renderer='string')
+    @view_config(route_name='wpis_show', renderer='docforge:templates/wpis_list.jinja2')
+    def show(self):
+        id = self.request.matchdict.get('id', None)
+        print id
+        items = [i.items() for i in self._list(id)]
+        return {'items': items}
+
