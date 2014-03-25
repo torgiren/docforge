2ff7b3c0ce4467a89f9c74145c29c767bfa5aa69
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2014-03-25 05:45:54 +0100

    wyświetlanie wniosków. ZŁY BRANCH. kilka poprzednich commitow jest nie do tego brancha

--- docforge/views/formatki.py
+++ docforge/views/formatki.py
@@ -16,12 +16,12 @@ class FormatkiView(object):
     def _list(self):
         return self.db.find()
 
-    def _add(self, name, zipped):
-        obj = {}
-        obj['_nazwa'] = name
-        for z in zipped:
-            obj["pole_%d" % z[0]] = z[1:]
-        self.db.insert(obj)
+#    def _add(self, name, zipped):
+#        obj = {}
+#        obj['_nazwa'] = name
+#        for z in zipped:
+#            obj["pole_%d" % z[0]] = z[1:]
+#        self.db.insert(obj)
 
     def _merge_field(self, post):
         """Wyciąga ze słownika post pola "nazwa_" oraz "typ",
@@ -47,37 +47,42 @@ class FormatkiView(object):
 
     @view_config(route_name='formatki_list', renderer='docforge:templates/list.jinja2')
     def list(self):
-        items = [{'verbose': unicode(i['_nazwa']), 'href': self.request.route_path('formatki_fill_form', id=i['_id'])} for i in self._list()]
+        items = [{'verbose': unicode(i['nazwa']), 'href': self.request.route_path('formatki_fill_form', id=i['_id'])} for i in self._list()]
         return {'items': items, 'title': u'Lista dostępnych formularzy'}
 
     @view_config(route_name='formatki_add_form', renderer='docforge:templates/formatki_form.jinja2')
     def show_form(self):
         return {}
 
-    @view_config(route_name='formatki_add_do', renderer='string')
-    def add_formatka(self):
-        if not self.request.POST.get('_nazwa'):
-            raise NoFormNameFoundException
-        zipped = self._merge_field(self.request.POST)
-        self._add(self.request.POST['_nazwa'], zipped)
-        return self.request.POST
+#    @view_config(route_name='formatki_add_do', renderer='string')
+#    def add_formatka(self):
+#        if not self.request.POST.get('_nazwa'):
+#            raise NoFormNameFoundException
+#        zipped = self._merge_field(self.request.POST)
+#        self._add(self.request.POST['_nazwa'], zipped)
+#        return self.request.POST
 
     @view_config(route_name='formatki_fill2_form', renderer='string')
     def fill2_form(self):
         tree = form.minidom.parse('/home/torgiren/docforge/docforge/playground/doc.xml')
         return form.print_c(tree.childNodes[0])
 
-    @view_config(route_name='formatki_fill_form', renderer='docforge:templates/formatki_fill.jinja2')
+    #@view_config(route_name='formatki_fill_form', renderer='docforge:templates/formatki_fill.jinja2')
+    @view_config(route_name='formatki_fill_form', renderer='docforge:templates/raw.jinja2')
     def fill_form(self):
+        print "Start"
         id = self.request.matchdict['id']
         obj = self._get(id)
-        nazwa = obj['_nazwa']
-        del(obj['_nazwa'])
-        del(obj['_id'])
-        items = [{'nazwa': j[2], 'widget': j[1], 'verbose': j[0]}
-                 for (i, j) in obj.items()]
-        return {'nazwa': nazwa, 'items': items, '_formatka': id}
-
+        nazwa = obj['nazwa']
+        template = obj['template']
+ #       del(obj['nazwa'])
+ #       del(obj['_id'])
+ #       items = [{'nazwa': j[2], 'widget': j[1], 'verbose': j[0]}
+ #                for (i, j) in obj.items()]
+ #       print "Koniec"
+ #       return {'nazwa': nazwa, 'items': items, '_formatka': id}
+        print template
+        return {'html': template, 'imie': "Marcin"}
     @view_config(route_name='formatki_fill_do', renderer='docforge:templates/list.jinja2')
     def fill(self):
         print "Jestem tutaj"
